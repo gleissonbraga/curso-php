ce43e8821df2ecbb76ab1f9d5e4857b49c2a7040
Gleisson Braga <bragagleisson@gmail.com>
2024-02-05 22:01:40 -0300

    Inicio do Curso

---
+++ exercicio.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@200..700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="assets/css/estilo.css">
+    <link rel="stylesheet" href="assets/css/exercicio.css">
+    <title>Exercício</title>
+</head>
+<body class="exercicio">
+
+    <header class="cabecalho">
+        <h1>Curso PHP</h1>
+        <h2>Visualização do Exercício</h2>
+    </header>
+
+    <nav class="navegacao">
+        <a href="#" class="verde">Sem formatação</a>
+        <a href="index.php" class="vermelho">Voltar</a>
+    </nav>
+
+    <main class="principal">
+        <div class="conteudo">
+
+        </div>
+    </main>
+
+    <footer class="rodape">
+        <p>COD3R & ALUNOS <?= date('y'); ?></p>
+    </footer>
+
+</body>
+</html>
